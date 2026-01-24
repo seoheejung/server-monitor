432eaa838e39162198a449bbaf01cfe861c69982
seoheejung <heejung0520@naver.com>
2026-01-25 00:01:51 +0900

    is_container_environment 수정

--- web/app/core/config.py
+++ web/app/core/config.py
@@ -1,6 +1,4 @@
 import platform
-from app.utils.env import is_container_environment
 
 # 서버 시작 시 한 번만 결정
 OS_TYPE = platform.system()
-IS_CONTAINER = is_container_environment()
--- web/app/utils/env.py
+++ web/app/utils/env.py
@@ -1,6 +1,10 @@
 import logging
 
-logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+
 
 def is_container_environment() -> bool:
     """
