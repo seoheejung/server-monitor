64023e6245d511b432daaa08ad3f75f2d0ee456b
seoheejung <heejung0520@naver.com>
2026-01-25 00:05:01 +0900

    is_container_environment 수정

--- web/app/main.py
+++ web/app/main.py
@@ -1,3 +1,11 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+
+
 from fastapi import FastAPI, Request, Response, status
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
--- web/app/utils/env.py
+++ web/app/utils/env.py
@@ -1,10 +1,6 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-)
-
+logger = logging.getLogger(__name__)
 
 def is_container_environment() -> bool:
     """
